b0934a4800f27d5ebcacb0a244dc33eb8f9e566a
BabaikaRub <rodionov.mihail02@mail.ru>
2022-06-10 18:28:31 +0300

    first commit

---
+++ coolsite/coolsite/urls.py
@@ -0,0 +1,29 @@
+"""coolsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from cgitb import handler
+from django.contrib import admin
+from django.urls import path, include # Функция include позволяет импрортировать url-адреса прямо из приложения,
+# чтобы они были независимыми друг от друга
+
+from women.views import pageNotFound
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('women.urls')), # Прописываем url-адрес для каждой функции
+]
+
+# Обработчик исключений для страницы 404 при выключенном DEBUG
+handler404 = pageNotFound
---
+++ coolsite/women/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+
+# Create your views here.
+
+def index(request): #HttpRequest
+    return HttpResponse('Страница приложения women.')
+
+
+# Вторым аргументом в функцию передаем параметр catid, который отвечает за переход по категориям
+# Этот параметр так же указываем в urls.py для данного приложения
+def categories(request, catid):
+    # Проверям, есть ли данные в GET запросе
+    if request.GET:
+        print(request.GET) # отлавливаем GET запрос
+
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+
+
+def archive(request, year):
+    # Генерируем ошибку 404 в другой функции. Произойдет перенаправление на функцию pageNotFound
+    # if int(year) > 2020:
+    #     raise Http404()
+
+    # Создаем редирект (перенаправление)
+    if int(year) > 2020:
+        return redirect('home', permanent=True) # по умолчанию генерится редирект с кодом 302, который говорит, что адрес страницы поменялся временно
+        # постоянный редирект можно сгенерить с помощью доп. параметра permanent (тогда код ответа будет 301)
+
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+
+# Функция обработчик исключения для ошибки 404, должна принимать 2 аргумента
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
