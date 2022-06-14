aff30b626203bcd624ce28ecd411f5b2adde2cf0
BabaikaRub <rodionov.mihail02@mail.ru>
2022-06-14 16:05:09 +0300

    add roots for site pages

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 
-from .views import index, categories, archive  # импортируем функции представления
+from .views import index, categories, archive, about  # импортируем функции представления
 
 
 urlpatterns = [
     path('', index, name='home'), # для каждого адреса будет добавляться префикс women, так как мы указали его в главном url.py проекта
-    path('cats/<int:catid>/', categories), # добавляем числовой параметр для перехода по категориям по шаблону <тип данных: имя>
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive), # формируем url-адрес с помощью регулярного выражения
+    #path('cats/<int:catid>/', categories), # добавляем числовой параметр для перехода по категориям по шаблону <тип данных: имя>
+    #re_path(r'^archive/(?P<year>[0-9]{4})/', archive), # формируем url-адрес с помощью регулярного выражения
+    path('about/', about, name='about'),
 ]
